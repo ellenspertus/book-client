c87401b64b8b8073455d629c6412f629b232244b
Ellen Spertus <spertus@mills.edu>
2020-11-03 15:32:37 -0800

    Start implementing wordOfTheDay

---
+++ src/main/java/edu/mills/cs180a/wordnik/WordOfTheDay.java
@@ -0,0 +1,19 @@
+package edu.mills.cs180a.wordnik;
+
+public class WordOfTheDay {
+    private String word;
+
+    public WordOfTheDay() {}
+
+    public WordOfTheDay(String word) {
+        this.word = word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+}
