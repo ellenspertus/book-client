4b8a1b0551b016ac0f591660aab21d0c512113e0
Ellen Spertus <spertus@mills.edu>
2020-11-03 15:56:32 -0800

    Load API key

--- src/main/java/edu/mills/cs180a/wordnik/WordnikImpl.java
+++ src/main/java/edu/mills/cs180a/wordnik/WordnikImpl.java
@@ -1,11 +1,22 @@
 package edu.mills.cs180a.wordnik;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import org.apache.commons.io.IOUtils;
 import feign.Feign;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
 
 public class WordnikImpl {
     private static final String URI_WORDNIK = "https://api.wordnik.com/v4";
+    private final String apiKey;
+
+    public WordnikImpl() throws IOException {
+        try (InputStream is = Main.class.getClassLoader().getResourceAsStream("api-key.txt")) {
+            apiKey = IOUtils.toString(is, StandardCharsets.UTF_8);
+        }
+    }
 
     public WordOfTheDay getWordOfTheDay() {
         WordnikResource wordnikResource = Feign.builder().encoder(new JacksonEncoder())
@@ -13,9 +24,8 @@ public class WordnikImpl {
         return wordnikResource.getWordOfTheDay();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         WordnikImpl wordnikImpl = new WordnikImpl();
         System.out.println(wordnikImpl.getWordOfTheDay());
     }
-
 }
